7bb56b7eaceaa7acc234c4d2bd4f75db1996c0d4
thezqc <thezqc@hotmail.com>
2016-11-13 10:05:09 +0800

    commit

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,3 +1,7 @@
 (add-to-list 'load-path (expand-file-name "~/.emacs.d/site-lisp"))
 (autoload 'php-mode "php-mode" "Major mode for editing php code." t)
 (add-to-list 'auto-mode-alist '("\\.php$" . php-mode))
+
+(setq make-backup-files nil)
+
+(if (file-exists-p "~/.emacs.d/custom.el") (load-file "~/.emacs.d/custom.el"))
